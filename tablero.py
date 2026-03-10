737a7d5125f159b52311a76ade94fadb67052cf8
boteroferradas <boteroferradas@danielcastelao.org>
2026-03-10 13:21:20 +0100

    Base del juego

---
+++ tablero.py
@@ -0,0 +1,12 @@
+class Tablero:
+    def __init__(self, tamanho = 10):
+        self._tamanho = tamanho
+        self.AGUA = 0
+        self.TOCADO = 1
+
+    def recibir_disparo(self):
+        pass
+    def colocar_nave(self, nave):
+        pass
+    def comprobar_impacto(self,x,y):
+        pass
